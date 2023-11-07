45e19297c1ed320ca55ff36044f5b97ca6e046b4
limengdu <37475446+limengdu@users.noreply.github.com>
2023-11-07 14:47:22 +0800

    first commit

---
+++ coin_prj.ino
@@ -0,0 +1,55 @@
+#define COIN_PIN D3                  // 投币机连接引脚
+#define PHOTO_BTN_PIN D4             // 拍照按钮连接引脚
+#define PRINT_BTN_PIN D5             // 打印按钮连接引脚
+
+#define DEBUG  1                     // 调试开关
+
+int countcoin = 0;                   // 投币计数
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(COIN_PIN, INPUT_PULLUP);
+  pinMode(PHOTO_BTN_PIN, INPUT_PULLUP);
+  pinMode(PRINT_BTN_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int coin_vol = digitalRead(COIN_PIN);
+  if(coin_vol != 0){
+    countcoin++;
+    if(DEBUG)Serial.println(String(countcoin));
+    if(countcoin == 3){
+      Serial.println("start");
+      while(1){
+//        Serial.println(digitalRead(PHOTO_BTN_PIN));
+        if(digitalRead(PHOTO_BTN_PIN) == 0){             // 拍照按键消抖+按下判断
+          delay(50);
+          if(digitalRead(PHOTO_BTN_PIN) == 0){
+            delay(50);
+            if(digitalRead(PHOTO_BTN_PIN) == 0){
+              Serial.println("photo");
+              break;
+            }
+          }
+        }
+
+        if(digitalRead(PRINT_BTN_PIN) == 0){             // 打印按键消抖+按下判断
+          delay(50);
+          if(digitalRead(PRINT_BTN_PIN) == 0){
+            delay(50);
+            if(digitalRead(PRINT_BTN_PIN) == 0){
+              Serial.println("print");
+              break;
+            }
+          }
+        }
+
+      }
+      countcoin = 0;                                     // 归位，重新投币计算
+    }
+  }
+  delay(20);                                             // 最短消抖时间，勿调
+}
