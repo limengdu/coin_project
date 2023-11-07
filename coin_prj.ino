078e880666f4f0dfe7498c9578dcf4fb33a4ca44
limengdu <37475446+limengdu@users.noreply.github.com>
2023-11-07 16:46:59 +0800

    sencond

--- coin_prj.ino
+++ coin_prj.ino
@@ -1,6 +1,7 @@
 #define COIN_PIN D3                  // 投币机连接引脚
 #define PHOTO_BTN_PIN D4             // 拍照按钮连接引脚
-#define PRINT_BTN_PIN D5             // 打印按钮连接引脚
+#define PRINT_BTN_PIN D4             // 打印按钮连接引脚
+#define RES_PIN D0                   // 复位引脚
 
 #define DEBUG  1                     // 调试开关
 
@@ -13,6 +14,12 @@ void setup() {
   pinMode(COIN_PIN, INPUT_PULLUP);
   pinMode(PHOTO_BTN_PIN, INPUT_PULLUP);
   pinMode(PRINT_BTN_PIN, INPUT_PULLUP);
+  pinMode(RES_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RES_PIN), resetDevice, FALLING);  // 按键中断
+}
+
+void ICACHE_RAM_ATTR resetDevice() {
+  ESP.restart();
 }
 
 void loop() {
@@ -31,22 +38,22 @@ void loop() {
             delay(50);
             if(digitalRead(PHOTO_BTN_PIN) == 0){
               Serial.println("photo");
+              while(1){
+                if(digitalRead(PRINT_BTN_PIN) == 0){             // 打印按键消抖+按下判断
+                  delay(50);
+                  if(digitalRead(PRINT_BTN_PIN) == 0){
+                    delay(50);
+                    if(digitalRead(PRINT_BTN_PIN) == 0){
+                      Serial.println("print");
+                      break;
+                    }
+                  }
+                }
+              }
               break;
             }
           }
         }
-
-        if(digitalRead(PRINT_BTN_PIN) == 0){             // 打印按键消抖+按下判断
-          delay(50);
-          if(digitalRead(PRINT_BTN_PIN) == 0){
-            delay(50);
-            if(digitalRead(PRINT_BTN_PIN) == 0){
-              Serial.println("print");
-              break;
-            }
-          }
-        }
-
       }
       countcoin = 0;                                     // 归位，重新投币计算
     }
